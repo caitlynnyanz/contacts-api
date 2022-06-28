dc00be76478f0f15020eb97c2621364dba643ff3
Caitlynn Yanz <caitlynnyanz601@gmail.com>
2022-06-28 17:43:03 -0600

    commiting seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Contact.create(first_name: "Jim", last_name: "Halpert", email: "jim.halpert@test.com", phone_number: "2086579943")
+
+Contact.create(first_name: "Pam", last_name: "Beasley", email: "pam.beasley@test.com", phone_number: "2086579943")
+
+Contact.create(first_name: "Michael", last_name: "Scott", email: "michael.scott@test.com", phone_number: "2086579943")
+
+Contact.create(first_name: "Dwight", last_name: "Schrute", email: "dwight.schrute@test.com", phone_number: "2086579943")
+
+puts "Done!"
